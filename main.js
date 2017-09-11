baf224f3e9d898a4a93f646078ca2623fe38a941
Rachel <reyu@uci.edu>
2017-09-11 16:13:40 -0700

    Same

--- main.js
+++ main.js
@@ -87,7 +87,7 @@
   <!-- About Section -->
   <div class="w3-container w3-padding-32" id="about">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About Me</h3>
-    <p>Hi! My name is Rachel Yu. I'm currently a 4th year computer science student at University of California, Irvine. 
+    <p>Hi! My name is Rachel Yu. I'm currently a 4th year computer science student at University of California, Irvine.</p>
 
     <!--I'm on track to graduate by December this year, 2017, so I'm quite excited for that.</p>
     <p>If it isn't obvious enough, I love dogs. I love animals in general, but dogs hold a special place in my heart. If I'm not looking at dog videos or playing with my own dogs, I'm hacking, playing video games, drawing, or discovering new things on the internet. </p>
